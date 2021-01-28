cfeb8a656c2d19bf12c0bba43393697fa65d5719
shivamnarayan6495 <77800690+shivamnarayan6495@users.noreply.github.com>
2021-01-28 16:24:42 +0530

    initial commit

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num1,num2,result;
+cout<<"enter first number:";
+cin>>num1;
+cout<<"enter second number:";
+cin>>num2;
+result=num1+num2;
+cout<<"addition is:"<<result;
+return 0;
+}
